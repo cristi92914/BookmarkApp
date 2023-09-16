a8b05021f681f0d5fa286670d9919cf4c9906c01
Balan Tudor-Cristian <balan.tudor.cristian@gmail.com>
2023-09-16 15:20:56 +0300

    localStorage functionality

--- index.js
+++ index.js
@@ -4,6 +4,45 @@ const site = document.getElementById("site");
 const bookmarks = document.getElementById("bookmarks");
 const form = document.querySelector("form");
 
+const jsonLocalStorage = localStorage.getItem("bookmarks");
+const bookmarksArr = JSON.parse(jsonLocalStorage) || [];
+
+const saveBookmarks = () => {
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarksArr));
+};
+
+const addDiv = (nameValue, siteValue, id) => {
+  const newDiv = document.createElement("div");
+  const newP = document.createElement("p");
+  const buttonDelete = document.createElement("button");
+  const buttonVisit = document.createElement("button");
+  buttonDelete.textContent = "delete";
+  buttonVisit.textContent = "visit";
+
+  //   const id = bookmarksArr?.length || 0;
+  buttonDelete.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.remove();
+
+    bookmarksArr.splice(id, 1);
+    console.log(id, bookmarksArr);
+    saveBookmarks();
+  });
+
+  buttonVisit.addEventListener("click", (e) => {
+    window.location.href = siteValue;
+  });
+
+  newP.textContent = nameValue;
+  bookmarks.appendChild(newDiv);
+  newDiv.appendChild(newP);
+  newDiv.appendChild(buttonDelete);
+  newDiv.appendChild(buttonVisit);
+};
+
+bookmarksArr?.forEach((element) => {
+  addDiv(element.name, element.url);
+});
+
 button.addEventListener("click", (e) => {
   e.preventDefault();
   form.reportValidity();
@@ -12,27 +51,15 @@ button.addEventListener("click", (e) => {
   const siteValue = site.value;
 
   if (!nameValue || !siteValue) return;
+  const id = bookmarksArr?.length || 0;
 
-  const newDiv = document.createElement("div");
-  const newP = document.createElement("p");
-  const buttonDelete = document.createElement("button");
-  const buttonVisit = document.createElement("button");
-  buttonDelete.textContent = "delete";
-  buttonVisit.textContent = "visit";
+  addDiv(name.value, site.value, id);
 
-  buttonDelete.addEventListener("click", (e) => {
-    e.currentTarget.parentElement.remove();
-  });
+  bookmarksArr.push({ id: id, name: name.value, url: site.value });
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarksArr));
 
-  buttonVisit.addEventListener("click", (e) => {
-    window.location.href = siteValue;
-  });
-
-  newP.textContent = nameValue;
   site.value = "";
   name.value = "";
-  bookmarks.appendChild(newDiv);
-  newDiv.appendChild(newP);
-  newDiv.appendChild(buttonDelete);
-  newDiv.appendChild(buttonVisit);
+
+  console.log(bookmarksArr);
 });
